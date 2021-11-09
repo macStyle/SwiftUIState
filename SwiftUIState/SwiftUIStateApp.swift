1035e4efe12b25acdaa439b857c94151bf9302ec
med <theoutsider009@gmail.com>
2021-11-09 19:12:23 +0100

    Initial Commit

---
+++ SwiftUIState/SwiftUIStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStateApp.swift
+//  SwiftUIState
+//
+//  Created by Adam Jemni on 11/9/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
